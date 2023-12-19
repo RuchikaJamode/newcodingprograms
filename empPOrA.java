deb677d242bb1aa7af66fe2d5351ef4955e93368
RuchikaJamode <ruchikasjamode@gmail.com>
2023-12-19 21:03:18 +0530

    present

---
+++ empPOrA.java
@@ -0,0 +1,25 @@
+package bridgelabz;
+import java.util.Scanner;
+
+public class empPOrA{
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+System.out.println("enter a number:");
+
+		
+		int randomno=sc.nextInt();
+	
+		if(randomno==1) {
+			System.out.println("employee is present");
+			
+		}
+		else {
+		
+		System.out.println("employee is absent");
+	}
+	}
+}
