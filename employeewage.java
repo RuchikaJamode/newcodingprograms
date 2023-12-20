4505c7a1b80e74cc3db64c7933759661fe2f2ab8
RuchikaJamode <ruchikasjamode@gmail.com>
2023-12-20 12:00:21 +0530

    present

--- employeewage.java
+++ employeewage.java
@@ -1,16 +1,14 @@
-package bridgelabz;
+package com.bridgelabz.employeewage;
 import java.util.Scanner;
 
-public class empPOrA{
+public class employeewage{
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-System.out.println("enter a number:");
-
-		
+		System.out.println("enter a empid:");
 		int randomno=sc.nextInt();
 	
 		if(randomno==1) {
